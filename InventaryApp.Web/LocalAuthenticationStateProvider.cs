175a346df76aa2c2e0b94869d3d36e285449be28
Maximo Jimeno <maximojimeno@gmail.com>
2020-11-27 23:14:00 -0400

    Fix Redirect to Login and Index

--- InventaryApp.Web/LocalAuthenticationStateProvider.cs
+++ InventaryApp.Web/LocalAuthenticationStateProvider.cs
@@ -31,8 +31,9 @@ namespace InventaryApp.Web
 
                 var identity = new ClaimsIdentity(claims, "BearerToken");
                 var user = new ClaimsPrincipal(identity);
-
-                return new AuthenticationState(user);
+                var state = new AuthenticationState(user);
+                NotifyAuthenticationStateChanged(Task.FromResult(state));
+                return state;
             }
 
             return new AuthenticationState(new ClaimsPrincipal());
