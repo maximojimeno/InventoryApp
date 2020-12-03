5b12c1f66bf675a5f00e935c8ed43b4152d0f06e
Lewis Gabin Fermin <lewis_gabin@ucne.edu.do>
2020-12-03 18:33:45 -0400

    Fix create product

---
+++ InventaryApp.Shared/CategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventaryApp.Shared
+{
+   public class CategoryViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+    
+
+
+}
