c4604415c0dc3be7937889ff2f603e6b49342f23
Maximo Eliezer Jimeno Disla <maximo.jimeno@hotmail.com>
2020-12-06 19:48:44 -0800

    Add CRUD Controller Brand and Services

---
+++ InventaryApp.Shared/Brand/BrandCollectionPagingResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventaryApp.Shared.Brand
+{
+    public class BrandCollectionPagingResponse : BaseApiResponse
+    {
+        public BrandViewModel[] Records { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int? NextPage { get; set; }
+        public int Count { get; set; }
+    }
+}
